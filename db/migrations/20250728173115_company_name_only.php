c865921b28776144479d43fcd5ec7a1ccf8ab044
Vítězslav Dvořák <vitezslav.dvorak@spojenet.cz>
2025-07-31 20:30:55 +0200

    RunTemplate interval now can be an Crontab expresion

--- db/migrations/20250728173115_company_name_only.php
+++ db/migrations/20250728173115_company_name_only.php
@@ -2,6 +2,17 @@
 
 declare(strict_types=1);
 
+/**
+ * This file is part of the MultiFlexi package
+ *
+ * https://multiflexi.eu/
+ *
+ * (c) Vítězslav Dvořák <http://vitexsoftware.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
 use Phinx\Migration\AbstractMigration;
 
 final class CompanyNameOnly extends AbstractMigration
@@ -14,10 +25,12 @@ final class CompanyNameOnly extends AbstractMigration
     public function change(): void
     {
         $table = $this->table('company');
+
         // Remove the composite index 'abraflexi' (on server, company)
         if ($table->hasIndex(['server', 'company'])) {
             $table->removeIndex(['server', 'company']);
         }
+
         $table->removeColumn('company')->update();
     }
 }
